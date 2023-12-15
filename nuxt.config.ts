23e1d429a58893f4d7b1371bb08719bf2f31b39a
Zain Wania <zain@wania.dev>
2023-12-15 09:39:07 -0500

    pre-rendered

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const pokemonArray = [];
+
+for (let i = 1; i <= 1011; i++) {
+  pokemonArray.push("/pokemon/" + i);
+}
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/image'],
@@ -6,4 +11,14 @@ export default defineNuxtConfig({
     typeCheck: true,
     shim: false,
   },
+  routeRules: {
+    '/': { isr: true},
+    '/pokemon/**': { isr: true },
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: pokemonArray
+    }
+  }
 })
